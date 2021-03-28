eb73851deb8a0662b8a2e76a2bf0c791586721b2
Sergii Maksymenko <sergiomaksymenko@gmail.com>
2021-03-28 15:07:47 +0200

    upd from bash

---
+++ src/js/menu-cards.js
@@ -0,0 +1,39 @@
+import data from '../data/menu.json';
+import template from '../templates/template.hbs';
+
+const refs = {
+  body: document.body,
+  list: document.getElementsByClassName('js-menu')[0],
+  input: document.querySelector('.theme-switch__toggle'),
+};
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+refs.body.classList.add(
+  localStorage.getItem('theme') === null
+    ? Theme.LIGHT
+    : localStorage.getItem('theme'),
+);
+
+const markup = template(data);
+
+refs.input.checked = localStorage.getItem('theme') === Theme.DARK;
+refs.list.insertAdjacentHTML('beforeend', markup);
+refs.input.addEventListener('change', inputChangeOn);
+
+function changeTheme(add, rem) {
+  refs.body.classList.remove(rem);
+  refs.body.classList.add(add);
+  localStorage.setItem('theme', add);
+}
+
+function inputChangeOn({ target }) {
+  if (target.checked) {
+    changeTheme(Theme.DARK, Theme.LIGHT);
+  } else {
+    changeTheme(Theme.LIGHT, Theme.DARK);
+  }
+}
